68807c117f745056f293188a8df5c1f1d824c5c8
Christina <christina_eberharter@hotmail.com>
2015-09-27 22:10:44 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL                                   #sets the value of s to NULL --> provides a default if cacheSolve has not been used yet
+  y <- NULL                                   #sets the value of y to NULL --> provides a default if cacheSolve has not been used yet
+  setmatrix <- function(y) {                  #sets the value of the matrix
+    x <<- y                                   #caches the matrix in order that cacheSolve can check whether it has changed
+    s <<- NULL                                #sets the value of s to NULL
+  }
+  getmatrix <- function() x                   #defines the getmatrix function
+  setinverse <- function(solve) s <<- solve   #defines inverse function solve
+  getinverse<- function() s                   #defines get inverse function
+  list(setmatrix = setmatrix, getmatrix = getmatrix, setinverse = setinverse, getinverse = getinverse)  #creates a list to store the functions
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getinverse()                         #if an inverse has already been contucted this will get it    
+  if(!is.null(s)) {                           #checks if cacheSolve has been run before
+    message("getting cached data")            #if cacheSolve already run, it will return above the matrix output the text "getting cached data"
+    return(s)                                 #returns the inverse
+  }
+  y<- x$getmatrix()                           #runs the getmatrix function to get the value of the input matrix
+  x$setmatrix(y)                              #runs the setmatrix function to cache it
+  s <- solve(y, ...)                          #computes the inverse value of the input matrix
+  x$setinverse(s)                             #runs the setinverse function 
+  s                                           #returns the inverse
 }
